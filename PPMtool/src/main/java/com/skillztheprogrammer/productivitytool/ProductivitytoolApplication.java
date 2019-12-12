12071489633585fad305c9b33d0d118682041bcc
unknown <skillztheprogrammer@gmail.com>
2019-12-12 23:01:57 +0100

    my first commit

---
+++ PPMtool/src/main/java/com/skillztheprogrammer/productivitytool/ProductivitytoolApplication.java
@@ -0,0 +1,13 @@
+package com.skillztheprogrammer.productivitytool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductivitytoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductivitytoolApplication.class, args);
+    }
+
+}
---
+++ PPMtool/src/test/java/com/skillztheprogrammer/productivitytool/ProductivitytoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skillztheprogrammer.productivitytool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductivitytoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
